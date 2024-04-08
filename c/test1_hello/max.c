47f21d28c4f0f4239a017709509a0961a73f4b80
GKNEETIEMAY <2780060291@qq.com>
2024-04-09 02:47:57 +0800

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	new file:   .vscode/launch.json
    	new file:   .vscode/tasks.json
    	deleted:    c/test1_hello/a.exe
    	new file:   test1_hello/a.exe
    	renamed:    c/test1_hello/hello_world.c -> test1_hello/hello_world.c
    	renamed:    c/test1_hello/max.c -> test1_hello/max.c
    	renamed:    c/test1_hello/max.h -> test1_hello/max.h
    	new file:   test1_hello/min.c
    	new file:   test1_hello/min.h

--- c/test1_hello/max.c
+++
@@ -1,7 +0,0 @@
-int Max(int a, int b)
-{
-    if (a>b)
-    return a;
-    else
-    return  b;
-}
---
+++ test1_hello/min.c
@@ -0,0 +1,7 @@
+int Min(int a, int b)
+{
+    if (a>b)
+    return b;
+    else
+    return  a;
+}
