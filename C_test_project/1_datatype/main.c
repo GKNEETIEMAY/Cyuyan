44b4c7ecd3977e0bfa2acc70aea07d9e132dd4cf
GKNEETIEMAY <2780060291@qq.com>
2024-04-15 01:14:29 +0800

    new file:   C_test_project/1_datatype/main.c 	new file:   C_test_project/1_datatype/main.exe

---
+++ C_test_project/1_datatype/main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+//了解数据类型所占的长度，单位：字节(Byte)
+//1 Byte = 8 bit
+int main(){
+    printf("char的长度:%d\n",sizeof(char));              //1
+    printf("short的长度:%d\n",sizeof(short));            //2
+    printf("int的长度:%d\n",sizeof(int));                //4
+    printf("long的长度:%d\n",sizeof(long));              //4
+    printf("long long的长度:%d\n",sizeof(long long));    //8
+    printf("float的长度:%d\n",sizeof(float));            //4
+    printf("double的长度:%d\n",sizeof(double));          //8
+    return 0;
+    
+}
