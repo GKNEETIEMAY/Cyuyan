4218c6c4a6b354f37c018579124d61460b159e13
GKNEETIEMAY <2780060291@qq.com>
2024-04-19 13:20:03 +0800

    modified:   C_test_project/2_VariablesandConstants/main.c 	modified:   C_test_project/2_VariablesandConstants/main.exe 	new file:   C_test_project/3_character_string/test.c 	new file:   C_test_project/3_character_string/test.exe

---
+++ C_test_project/3_character_string/test.c
@@ -0,0 +1,23 @@
+/***字符串：由双引号引起的一串字符
+ * 字符串以\0结尾，\0不能当作字符串字面值
+ *
+ *
+ * **/
+#include <stdio.h>
+int main()
+{
+    char arr1[] = "abc"; // 字符串:abc\0
+    char arr2[] = {'a', 'b', 'c', 0};
+    int i;
+    // printf("%s\n",arr1[0]);//error:Segmentation fault,原因是%c用于单个字符，%s用于整个字符串
+    printf("arr1=%s\n", arr1);
+    printf("arr2=%s\n", arr2);
+    arr2[0]='d';
+    printf("arr2=%s\n", arr2);
+    for ( i = 0; i < 4; i++)
+    {
+        printf("arr2[%d]=%c\n",i, arr2[i]);
+    }
+    
+    return 0;
+}
