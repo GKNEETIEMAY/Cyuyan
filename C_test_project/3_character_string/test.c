1b0d0ee00f4164e5077968b21663ad5702b961ba
GKNEETIEMAY <2780060291@qq.com>
2024-04-20 23:39:17 +0800

    modified:   C_test_project/3_character_string/test.c

--- C_test_project/3_character_string/test.c
+++ C_test_project/3_character_string/test.c
@@ -6,7 +6,7 @@
 #include <stdio.h>
 int main()
 {
-    char arr1[] = "abc"; // 字符串:abc\0
+    char arr1[] = "abc"; // 字符串:abc\0,\0为字符串结束的标志
     char arr2[] = {'a', 'b', 'c', 0};
     int i;
     // printf("%s\n",arr1[0]);//error:Segmentation fault,原因是%c用于单个字符，%s用于整个字符串
