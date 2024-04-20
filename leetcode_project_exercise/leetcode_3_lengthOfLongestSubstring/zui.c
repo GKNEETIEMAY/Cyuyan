3e5690e1f5609b18d00234ba8990ee31408c774f
GKNEETIEMAY <2780060291@qq.com>
2024-04-21 03:26:45 +0800

    modified:   C_test_project/3_character_string/test.c

--- leetcode_project_exercise/leetcode_3_lengthOfLongestSubstring/zui.c
+++ leetcode_project_exercise/leetcode_3_lengthOfLongestSubstring/zui.c
@@ -1,39 +1,61 @@
 #include <string.h>
 #include <stdio.h>
+#include<stdbool.h>
+bool ACU(char *str){
+    bool charexit[256]={false};
+    while(*str != 0){
+        unsigned char c=(unsigned char)*str;
+        if(charexit[c])
+        {return false;//有重复
+        }
+        charexit[c]=true;
+        str++;
+    }
+    return true;//无重复
+}
 int lengthOfLongestSubstring(char *s)
 {
     char *a = s;
     int i, j, lon, m, n;
-    lon = strlen(a) - 1; // 输入的字符串数组的长度
-    m = 0;               // 计数
-    n = lon; // 滑框尺寸
-    if (n < 0)          //防止出现""
+    char *o;
+    int p = 0;
+    //int charExists[256] = {0}; // 初始化一个数组来记录字符是否出现过，默认为0（false）
+    //bool allUnique = true;     // 假设所有字符都是唯一的
+    lon = strlen(a) - 1;       // 输入的字符串数组的长度
+    m = 0;                     // 
+    n = lon;                   // 滑框尺寸
+    if (n < 0)                 // 防止出现""
     {
-        return m ;
+        return m;
     }
     else
     {
-        for (; n > 1; n--)
+        for (; n >= 0; n--)
         {
-            printf("滑框长度为:%d\n",n);
-            for (i = 0; i <= lon - n; i++)
+            printf("滑框长度为:%d\n", n + 1);
+            for (i = 0; i <= lon - n; i++) // 滑框头
             {
-                printf("从a[%d]开始\n",i);
-                for (m = 0, j = i + n; j > i; j--)
+                printf("从a[%d]的尾巴开始，依次是\n", i);
+                for (p = 0, j = i + n; j >= i; j--) // 滑框尾
                 {
-                    if (a[i] != a[j])
-                    {
-                        m = m + 1;
-                        printf("m=%d\n", m);
-                    }
-                    if (m == n)
-                    {
-                        printf("此时的m==n,m+1=%d\n", m+1);//c此处有问题
-                        return m+1;
-                    }
+                    printf("%c\n", a[j]);
+                    o[p] = a[j];
+                    p = p + 1;
+                }
+                o[p] = 0;//加字符串结尾
+                printf("%s\n", o);
+                //////////////////
+                if(ACU(o))
+                {
+                    return n+1;
+                }
+                
+                /////////////////
+                for (; p >= 0; p--) // 清空字符串
+                {
+                    o[p] = 0;
                 }
             }
         }
-        return m+1;
     }
 }
--- leetcode_project_exercise/leetcode_3_lengthOfLongestSubstring/zuichangzichuan.c
+++ leetcode_project_exercise/leetcode_3_lengthOfLongestSubstring/zuichangzichuan.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 #include"zui.h"
 int main(){
-    char *s="abcb";
+    char *s="abcabcbb";
     int a;
     printf("%s\n",s);
     a=lengthOfLongestSubstring(s);
-    printf("%d\n",a);
+    printf("结束%d\n",a);
     return 0;
 }
