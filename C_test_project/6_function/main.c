07a44105f77865efe2febc0d8a2e78d25b31b077
GKNEETIEMAY <2780060291@qq.com>
2024-05-06 22:11:20 +0800

    modified:   C_test_project/6_function/main.c 	new file:   C_test_project/6_function/main.exe

--- C_test_project/6_function/main.c
+++ C_test_project/6_function/main.c
@@ -1,10 +1,37 @@
-//函数
+// 函数
 //
 #include <stdio.h>
+int MAX(int x,int y);//主函数在上时，要在最上方定义一下
 int main()
 {
-    int a, b;
-    a = 0;
-    b = 0;
+    int a, b, c;
+    printf("请输入第一个数字：\n");
+    scanf("%d", &a);
+    printf("请输入第二个数字：\n");
+    scanf("%d", &b);
+    if (a - b == 0)
+    {
+        printf("两数值相等\n");
+    }
+    else
+    {
+        c = MAX(a, b);
+        printf("较大值为:%d\n", c);
+    }
     return 0;
+}
+
+int MAX(int x, int y)
+{
+    int a;
+    if (x > y)
+    {
+        a = x;
+        return a;
+    }
+    else
+    {
+        a = y;
+        return a;
+    }
 }
