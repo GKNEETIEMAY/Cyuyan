a732b79fed314e096a34db9cf3a04910dd56e711
GKNEETIEMAY <2780060291@qq.com>
2024-05-10 15:50:36 +0800

    new file:   C_test_project/8_keyword/add.c 	modified:   C_test_project/8_keyword/kw.c 	modified:   C_test_project/8_keyword/kw.exe

---
+++ C_test_project/8_keyword/add.c
@@ -0,0 +1,2 @@
+int g_val1 = 2024; // 全局变量
+static g_val2 = 2024;
--- C_test_project/8_keyword/kw.c
+++ C_test_project/8_keyword/kw.c
@@ -1,64 +1,69 @@
-//关键字:不能跟变量名字冲突
-//auto:
-//break 
-//extern 
-//float 
-//for
-//sizeof
-//case 
-//char 
-//const 
-//continue 
-//default 
-//do 
-//double 
-//else 
-//enum
-//goto 
-//if 
-//it 
-//long 
-//register 
-//return 
-//short 
-//signed
-//static:修饰静态变量和静态函数
-//struct 
-//switch 
-//typedef:类型定义，可理解为类型重命名，ex:typedef unsigned int unit_32
-//union 
-//unsigned:无符号
-//void 
-//volatile 
-//while
-//注意define不是一种关键字
-#include<stdio.h>
-void test()//测试static函数
+// 关键字:不能跟变量名字冲突
+// auto:
+// break
+// extern:声明外部符号
+// float
+// for
+// sizeof
+// case
+// char
+// const
+// continue
+// default
+// do
+// double
+// else
+// enum
+// goto
+// if
+// it
+// long
+// register
+// return
+// short
+// signed
+// static:修饰静态变量和静态函数
+// struct
+// switch
+// typedef:类型定义，可理解为类型重命名，ex:typedef unsigned int unit_32
+// union
+// unsigned:无符号
+// void
+// volatile
+// while
+// 注意define不是一种关键字
+#include <stdio.h>
+// static修饰局部变量时
+void test()
 {
-    int a=1;
-    static int b=1;//b是一个静态局部变量
+    int a = 1;        // 会被销毁
+    static int b = 1; // b是一个静态局部变量，不会被销毁，局部变量的生命周期变长
     a++;
     b++;
     printf("注意比较\n");
-    printf("int a=%d\n",a);//2，2，2，2，2
-    printf("static int b=%d\n",b);//2，3，4，5，6
+    printf("int a=%d\n", a);        // 2，2，2，2，2
+    printf("static int b=%d\n", b); // 2，3，4，5，6
 }
-int main(){
+int main()
+{
 
-    //int float;//err
-    //关键字typedef
+    // int float;//err
+    // 关键字typedef
     typedef unsigned int unit_32;
-    unsigned int n1=1;
-    unit_32 n2=1;//n1=n2
-    if(n1==n2)
+    unsigned int n1 = 1;
+    unit_32 n2 = 1; // n1 = n2
+    if (n1 == n2)
     {
-        printf("重命名后，n1==n2\n");
+        printf("重命名后,n1==n2\n");
     }
-    //关键字static
-    for(int i=0;i<5;i++)
+    // 关键字static
+    for (int i = 0; i < 5; i++)
     {
         test();
     }
+    // 关键字extern
+    extern int g_val1;
+    printf("g_val1=%d\n", g_val1);
 
     return 0;
 }
