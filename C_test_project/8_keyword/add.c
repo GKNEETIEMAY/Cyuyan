fffc629da491f8eb9379536dd7fb4a714537dd6f
GKNEETIEMAY <2780060291@qq.com>
2024-05-10 16:55:17 +0800

    modified:   C_test_project/8_keyword/add.c

--- C_test_project/8_keyword/add.c
+++ C_test_project/8_keyword/add.c
@@ -5,11 +5,14 @@ int g_val1 = 2024; // 全局变量
 // 出了源文件无法使用
 static int g_val2 = 2024;
 //声明相加函数，可以被其他文件调用
+//普通的函数具有外部链接属性
 int Add(int a, int b)
 {
     return a+b;
 }
-//static声明的函数，
+//static声明的函数，改变了函数的链接属性
+// 让此函数只能在源文件内部使用
+// 出了源文件无法使用，只具有内部链接属性
 static int Sub(int a, int b)
 {
     return a-b;
