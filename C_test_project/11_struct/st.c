4740c8cf47f796156a0ea05c25f86db63efed536
GKNEETIEMAY <2780060291@qq.com>
2024-05-15 00:10:20 +0800

    modified:   C_test_project/11_struct/st.c 	modified:   C_test_project/11_struct/st.exe

--- C_test_project/11_struct/st.c
+++ C_test_project/11_struct/st.c
@@ -2,6 +2,7 @@
  * 结构体-描述复杂对象
  */
 #include <stdio.h>
+#include<string.h>
 struct Book
 {
     char name[20];   // 书名
@@ -21,5 +22,11 @@ int main()
     printf("a书的版号是:%d\n", (*pa).copyright); // 法1：结构体利用指针需要加括号
     //->操作符找到结构体变量的成员时：指针->成员
     printf("a书的作者是:%s\n", pa->author); // 法2：指针指向对象的内部成员
+
+    a.price=35.9;//变量可以直接更改
+    printf("a书的名字是:%s\n",a.name);
+    //a.name="c++";//数组不能直接更改
+    strcpy(a.name,"c++");
+    printf("更改后的a书的名字是:%s\n",a.name);
     return 0;
 }
