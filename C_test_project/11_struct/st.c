07025d18590ca27f3a85fc5a3488c8ed8b2f50e7
GKNEETIEMAY <2780060291@qq.com>
2024-05-14 23:49:24 +0800

    modified:   C_test_project/11_struct/st.c new file:   C_test_project/11_struct/st.exe

--- C_test_project/11_struct/st.c
+++ C_test_project/11_struct/st.c
@@ -1,21 +1,23 @@
 /***
  * 结构体-描述复杂对象
  */
-#include<stdio.h>
+#include <stdio.h>
 struct Book
 {
-    char name[20];
-    char author[20];
-    float price;
-    int copyright;
+    char name[20];   // 书名
+    char author[20]; // 作者
+    float price;     // 价格
+    int copyright;   // 版号
 
-}book;
+} book;
 
 int main()
 {
-    struct Book a={"cyuyan","lxl",20.5,10001};
-    printf("a书的价格是:%f\n",a.price);
-
+    struct Book a = {"cyuyan", "lxl", 20.5, 10001};//定义a书
+    //book b;
+    printf("a书的价格是:%.3f元\n", a.price);
+    struct Book *pa=&a;
+    printf("a书的版号是:%d\n", (*pa).copyright);
 
     return 0;
 }
