ec630ba136e0013b40b9322184c01568dd7fe840
GKNEETIEMAY <2780060291@qq.com>
2024-05-14 23:53:05 +0800

    modified:   C_test_project/11_struct/st.c 	modified:   C_test_project/11_struct/st.exe

--- C_test_project/11_struct/st.c
+++ C_test_project/11_struct/st.c
@@ -13,11 +13,11 @@ struct Book
 
 int main()
 {
-    struct Book a = {"cyuyan", "lxl", 20.5, 10001};//定义a书
-    //book b;
+    struct Book a = {"cyuyan", "lxl", 20.5, 10001}; // 定义a书
+    // book b;
     printf("a书的价格是:%.3f元\n", a.price);
-    struct Book *pa=&a;
-    printf("a书的版号是:%d\n", (*pa).copyright);
-
+    struct Book *pa = &a;
+    printf("a书的版号是:%d\n", (*pa).copyright); // 法1：结构体利用指针需要加括号
+    printf("a书的作者是:%s\n", pa->author); // 法2：指针指向位置
     return 0;
 }
