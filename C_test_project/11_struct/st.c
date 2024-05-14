56fb092800a75aa5fe8fc66076e77f39d9e99b0e
GKNEETIEMAY <2780060291@qq.com>
2024-05-15 00:14:45 +0800

    modified:   C_test_project/11_struct/st.c 	renamed:    C_test_project/4_if_else/main.c -> C_test_project/4_Branch/main.c 	renamed:    C_test_project/4_if_else/main.exe -> C_test_project/4_Branch/main.exe

--- C_test_project/11_struct/st.c
+++ C_test_project/11_struct/st.c
@@ -2,7 +2,7 @@
  * 结构体-描述复杂对象
  */
 #include <stdio.h>
-#include<string.h>
+#include <string.h>
 struct Book
 {
     char name[20];   // 书名
@@ -23,10 +23,10 @@ int main()
     //->操作符找到结构体变量的成员时：指针->成员
     printf("a书的作者是:%s\n", pa->author); // 法2：指针指向对象的内部成员
 
-    a.price=35.9;//变量可以直接更改
-    printf("a书的名字是:%s\n",a.name);
-    //a.name="c++";//数组不能直接更改
-    strcpy(a.name,"c++");
-    printf("更改后的a书的名字是:%s\n",a.name);
+    a.price = 35.9; // 变量可以直接更改
+    printf("a书的名字是:%s\n", a.name);
+    // a.name="c++";//数组不能直接更改
+    strcpy(a.name, "c++");
+    printf("更改后的a书的名字是:%s\n", a.name);
     return 0;
 }
