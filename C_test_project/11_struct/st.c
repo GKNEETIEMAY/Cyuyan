e004102790d868eff2ee8407a7ada851c92805db
GKNEETIEMAY <2780060291@qq.com>
2024-05-14 23:38:01 +0800

    modified:   C_test_project/11_struct/st.c

--- C_test_project/11_struct/st.c
+++ C_test_project/11_struct/st.c
@@ -2,7 +2,19 @@
  * 结构体-描述复杂对象
  */
 #include<stdio.h>
+struct Book
+{
+    char name[20];
+    char author[20];
+    float price;
+    int copyright;
+
+}book;
+
 int main()
 {
+    struct Book a={"cyuyan","lxl",20.5,10001};
+
+
     return 0;
 }
