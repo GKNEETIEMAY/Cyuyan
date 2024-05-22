98f7dc995ce0f9431ce66ba88e980768ae6b6706
GKNEETIEMAY <2780060291@qq.com>
2024-05-23 02:19:08 +0800

    每个小章节下面加入course 、 practice 两个部分

--- C_test_project/5_loop_statement/course/main.c
+++ C_test_project/5_loop_statement/course/main.c
@@ -4,7 +4,6 @@
     {
         code;
     }
-    
  * 2.for:
  * for (size_t i = 0; i < count; i++)
     {
@@ -17,25 +16,27 @@ do
     } while (condition);
   */
 
-#include<stdio.h>
-int main(){
-    int a=0;
-    //while
-    while (a<3)
+#include <stdio.h>
+int main()
+{
+    int a = 0;
+    // while
+    while (a < 3) // while(1)死循环
     {
-        printf("a=%d\n",a);
+        printf("a=%d\n", a);
         a++;
     }
-    //for
-    for(;a>0;a--){
-        printf("a=%d\n",a);
+    // for
+    for (; a > 0; a--)
+    {
+        printf("a=%d\n", a);
     }
-    //do while
+    // do while
     do
     {
-        printf("a=%d\n",a);
+        printf("a=%d\n", a);
         a++;
-    } while (a<3);
-    
+    } while (a < 3);
+
     return 0;
 }
