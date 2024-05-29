7df22a98aecde0f31280b9ba7b5365f6714f0dd1
GKNEETIEMAY <2780060291@qq.com>
2024-05-29 23:48:46 +0800

    leetcode解题 	new file:   "leetcode/4.\345\257\273\346\211\276\344\270\244\344\270\252\346\255\243\345\272\217\346\225\260\347\273\204\347\232\204\344\270\255\344\275\215\346\225\260.c"

---
+++ leetcode/4.寻找两个正序数组的中位数.c
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=4 lang=c
+ *
+ * [4] 寻找两个正序数组的中位数
+ */
+
+// @lc code=start
+double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size) {
+    int nums3Size=nums1Size+nums2Size;//定义合并后的数组长度
+    int i=0,j=0;//
+    int nums3[nums3Size];//定义合并后的数组
+    //归并排序
+
+    
+}
+// @lc code=end
+
