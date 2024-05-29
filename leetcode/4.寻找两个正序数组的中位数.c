ad5afdb67410998e40ebc0f2ece2079f9f7832c8
GKNEETIEMAY <2780060291@qq.com>
2024-05-30 02:36:13 +0800

    modified:   "leetcode/4.\345\257\273\346\211\276\344\270\244\344\270\252\346\255\243\345\272\217\346\225\260\347\273\204\347\232\204\344\270\255\344\275\215\346\225\260.c" 	new file:   "leetcode/5.\346\234\200\351\225\277\345\233\236\346\226\207\345\255\220\344\270\262.c"

--- leetcode/4.寻找两个正序数组的中位数.c
+++ leetcode/4.寻找两个正序数组的中位数.c
@@ -11,20 +11,20 @@ double findMedianSortedArrays(int *nums1, int nums1Size, int *nums2, int nums2Si
     int left = 0, middle = 0, right = 0;   // left对应1，right对应2，middle对应3
     int nums3[nums3Size];                  // 定义合并后的数组
     int i, j;                              // 定义中位数左右两边的数字的下标
-    double median;
+    double median;                         // 中位数
     // 归并排序
     for (; middle < nums3Size;)
     {
-        if (left < nums1Size && right < nums2Size)
+        if (left < nums1Size && right < nums2Size) // 如果两个合并前的有序数组均从小于两者最大长度开始比较
         {
-            if (nums1[left] < nums2[right])
+            if (nums1[left] < nums2[right]) // 哪个小哪个进入合并的数组
             {
                 nums3[middle] = nums1[left];
                 middle++;
                 left++;
                 continue;
             }
-            else if (nums1[left] == nums2[right])
+            else if (nums1[left] == nums2[right]) // 稳定的排序算法
             {
                 nums3[middle] = nums1[left];
                 nums3[middle + 1] = nums2[right];
@@ -41,22 +41,22 @@ double findMedianSortedArrays(int *nums1, int nums1Size, int *nums2, int nums2Si
                 continue;
             }
         }
-        //数组1空
-        else if (left>=nums1Size)
+        // 数组1空
+        else if (left >= nums1Size)
         {
-            nums3[middle]=nums2[right];
+            nums3[middle] = nums2[right];
             middle++;
             right++;
             continue;
         }
-        //数组2空
-        else if (right>=nums2Size)
+        // 数组2空
+        else if (right >= nums2Size)
         {
-            nums3[middle]=nums1[left];
+            nums3[middle] = nums1[left];
             middle++;
             left++;
             continue;
-        }   
+        }
     }
     // 寻找两个正序数组的中位数,需要判断合并数组的下标和是奇数还是偶数
     // 奇数个->最后一个下标偶数：12345->0-4->num/2->4/2=2
---
+++ leetcode/5.最长回文子串.c
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=5 lang=c
+ *
+ * [5] 最长回文子串
+ */
+
+// @lc code=start
+char *longestPalindrome(char *s)
+{
+    int left = 0, right = 0; // 字符串定义左右两个边界
+    int maxlength;           // 最长回文子串的长度
+    char z;//返回值
+    return z;
+}
+// @lc code=end
