e6c533592b120c9e4532a81322dd36f7d4c9db6d
GKNEETIEMAY <2780060291@qq.com>
2024-05-29 23:52:11 +0800

    题解

--- leetcode/4.寻找两个正序数组的中位数.c
+++ leetcode/4.寻找两个正序数组的中位数.c
@@ -7,9 +7,14 @@
 // @lc code=start
 double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size) {
     int nums3Size=nums1Size+nums2Size;//定义合并后的数组长度
-    int i=0,j=0;//
+    int left=0,middle=0,right=0;//left对应1，right对应2，middle对应3
     int nums3[nums3Size];//定义合并后的数组
     //归并排序
+    for(;;){
+
+
+    }
+    //寻找两个正序数组的中位数
 
     
 }
