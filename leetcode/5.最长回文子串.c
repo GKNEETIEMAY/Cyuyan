bf366754afb2508fa96e7818281de7ff8dcf441c
GKNEETIEMAY <2780060291@qq.com>
2024-05-31 03:34:50 +0800

    modified:   .vscode/settings.json 	modified:   C_test_project/10_pointer/p.c 	modified:   "leetcode/5.\346\234\200\351\225\277\345\233\236\346\226\207\345\255\220\344\270\262.c" 	modified:   leetcode_project_exercise/leetcode_3_lengthOfLongestSubstring/a.exe

--- leetcode/5.最长回文子串.c
+++ leetcode/5.最长回文子串.c
@@ -5,11 +5,36 @@
  */
 
 // @lc code=start
+#include <string.h>
 char *longestPalindrome(char *s)
-{
-    int left = 0, right = 0; // 字符串定义左右两个边界
-    int maxlength;           // 最长回文子串的长度
-    char z;//返回值
+{                           //"babad\0 -> bab||aba"
+    int length = strlen(s); // 字符串的长度
+    int n = length - 1;     // 滑块尺寸初始大小
+    int left = 0, right;    // 滑块两个边界
+    int zmaxlength;         // 最长回文子串的长度
+    int head;
+    char z[] = {'\0'}; // 返回值
+    // 思路：大滑动窗口，依次减小
+    for (; n > 0; n--) // 滑框进行减小的过程
+    {
+        for (; left < length - n; left++)
+        {
+            head = left;//记录此次的左边初始位置
+            right = left + n; // 定义右边的大小
+            while (s[left++] == s[right--])
+            {
+                if (left > right)
+                {
+                    for (int i = 0; i <= n; i++)
+                    {
+                        z[i] = s[head + i];
+                    }
+                    break;
+                }
+            }
+        }
+    }
+
     return z;
 }
 // @lc code=end
