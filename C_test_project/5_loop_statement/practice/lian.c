59022002a8e9e376c6ff8418c07326dc9d2c934b
GKNEETIEMAY <2780060291@qq.com>
2024-06-03 03:17:17 +0800

    modified:   C_test_project/5_loop_statement/course/main.c 	new file:   C_test_project/5_loop_statement/practice/a.exe 	new file:   C_test_project/5_loop_statement/practice/lian.c 	new file:   C_test_project/5_loop_statement/practice/lian.exe

---
+++ C_test_project/5_loop_statement/practice/lian.c
@@ -0,0 +1,33 @@
+/***
+ * 练习：
+ * 1.求n!
+ * 2.求1!+2!+...+10!
+ * 3.在有序数组v[n]中查找某个数字x
+ */
+#include<stdio.h>
+//递归求阶乘，解1，2
+int y(int x){
+    if(x>0){
+    return x*y(x-1);
+    }
+    else{
+        return 1;
+    }
+}
+//在有序数组v[n]中查找某个数字x
+int binsearch(int x,int v[],int n){
+
+}
+int main(){
+    int x;
+    printf("请输入一个数字x:\n");
+    scanf("%d",&x);
+    printf("%d的阶乘是:%d\n",x,y(x));
+    printf("--------------------------------\n");
+    int sum=0;
+    for(int i=1;i<11;i++){
+        sum=y(i)+sum;
+    }
+    printf("1!+2!+...+10!=%d\n",sum);
+    return 0;
+}
