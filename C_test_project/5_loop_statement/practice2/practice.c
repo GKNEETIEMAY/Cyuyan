4f30c108868df32a6ccc70b925597589f98692c0
GKNEETIEMAY <2780060291@qq.com>
2024-06-16 01:36:30 +0800

    renamed:    C_test_project/5_loop_statement/practice/a.exe -> C_test_project/5_loop_statement/practice1/a.exe 	renamed:    C_test_project/5_loop_statement/practice/lian.c -> C_test_project/5_loop_statement/practice1/lian.c 	new file:   C_test_project/5_loop_statement/practice2/a.exe 	new file:   C_test_project/5_loop_statement/practice2/practice.c

---
+++ C_test_project/5_loop_statement/practice2/practice.c
@@ -0,0 +1,24 @@
+/***
+ * 练习：
+ * 1.分数求和:1/1-1/2+1/3-1/4+...+1/99-1/100
+ * 2.求最大值
+ * 3.乘法口诀表
+ */
+#include <stdio.h>
+#include <math.h>
+int Sum_fraction()
+{
+    float sum=0;
+    for(int i=1;i<=100;i++)
+    {
+        sum=sum+(1/(pow(-1,i-1)*i));
+    }
+    printf("%f\n",sum);
+    return 0;
+}
+int main()
+{
+    Sum_fraction();
+    return 0;
+    //
+}
