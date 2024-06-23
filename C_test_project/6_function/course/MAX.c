2d6ab90bfcfd975c68dde7f81b209831d0c4b2b1
GKNEETIEMAY <2780060291@qq.com>
2024-06-24 02:04:46 +0800

    new file:   C_test_project/6_function/course/MAX.c 	new file:   C_test_project/6_function/course/Max.h 	modified:   C_test_project/6_function/course/a.exe 	modified:   C_test_project/6_function/course/main.c

---
+++ C_test_project/6_function/course/MAX.c
@@ -0,0 +1,11 @@
+int MAX(int x, int y) // 比较大小函数
+{
+    if (x > y)
+    {
+        return x;
+    }
+    else
+    {
+        return y;
+    }
+}
---
+++ C_test_project/6_function/course/Max.h
@@ -0,0 +1,5 @@
+#ifndef __MAX_H_
+#define __MAX_H_
+//函数的声明
+int MAX(int x, int y); // 比较大小函数
+#endif //__MAX_H_
