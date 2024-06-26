b6ce611affa80b752874804b2e57b210ee24340b
GKNEETIEMAY <2780060291@qq.com>
2024-06-27 01:20:59 +0800

    new file:   C_test_project/10_pointer/practice/a.exe 	modified:   C_test_project/10_pointer/practice/practice.c

--- C_test_project/10_pointer/practice/practice.c
+++ C_test_project/10_pointer/practice/practice.c
@@ -1,3 +1,6 @@
+/***
+ * 牛客：明明的随机数
+ */
 #include <stdio.h>
 //冒泡排序
 int B_sort(int* nums, int numsSize) {
@@ -16,26 +19,30 @@ int B_sort(int* nums, int numsSize) {
 //删除重复数字-哈希
 int Del(int* nums, int numsSize) {
     int hash[500];
+    int n[numsSize];
     for (int i = 0; i < 500; i++) {
         hash[i] = 0;
     }
-    //哈希
+    //哈希，第2次以上重复的给标记-1
     for (int i = 0; i < numsSize; i++) {
         hash[nums[i]] = hash[nums[i]] + 1;
         if (hash[nums[i]] > 1) {
             nums[i] = -1;
         }
     }
-    //整理数组,遇见-1，左移一位。numsSize-1
+        //整理数组,遇见-1，左移一位。
+    int j=0;
     for (int i = 0; i < numsSize; i++) {
-        if (nums[i] == -1) {
-            for (int j = i; i < numsSize - 1; i++) {
-                nums[j] = nums[j + 1];
+        if (nums[i]!= -1) {
+            n[j]=nums[i];
+            j++;
             }
-            numsSize = numsSize - 1;
         }
+    for (int i = 0; i < j; i++)
+    {
+        nums[i]=n[i];
     }
-    return numsSize;
+    return j;
 }
 int main() {
     int numsSize;//第一个数据N，即含有几个数字
@@ -45,7 +52,7 @@ int main() {
     for (int i = 0; i < numsSize; i++) {
         scanf("%d", &num[i]);
     }
-    int* nums = &num;
+    int* nums = num;
     numsSize = Del(nums, numsSize);
     B_sort(nums, numsSize);
     for (int i = 0; i < numsSize; i++) {
