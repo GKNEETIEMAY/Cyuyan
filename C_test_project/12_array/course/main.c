3298b43ade7f168a907f2abc851153352d5b6104
GKNEETIEMAY <2780060291@qq.com>
2024-06-29 20:37:28 +0800

    new file:   C_test_project/12_array/course/a.exe

---
+++ C_test_project/12_array/course/main.c
@@ -0,0 +1,13 @@
+/**
+ * 1.一维数组的创建与初始化
+ */
+#include<stdio.h>
+int main(){
+    int arr1[10]={};//一维数组的创建
+    int count =9;
+    int arr2[count];//
+    int arr3[10]={1,2,3};//一维数组的创建和不完全初始化，剩下的元素默认为0
+    printf("nu%d",arr3[0]);
+
+    return 0;
+}
